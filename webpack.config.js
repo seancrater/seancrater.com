b77813ab1e78dba295ca26381828800f9a39e4e5
seancrater <sean@seancrater.com>
2018-12-13 09:34:48 -0500

    Configuring to pull from environment variable

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,9 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new webpack.EnvironmentPlugin(['GIT_TOKEN'])
+    ],
     resolve: {
         extensions: ['*', '.js', '.jsx', '.mjs']
     },
